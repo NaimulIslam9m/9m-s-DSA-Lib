1023fcd0fb333870052bc6b381149056f5cd9d8d
Naimul Islam <naim2018331019@gmail.com>
2020-08-15 21:05:37 +0600

    -

---
+++ DP.cpp
@@ -0,0 +1,6 @@
+// dynamic programming techniques
+// dp techniques
+
+//A sequence of elements| 1 operation| Every elements have different cost for a operation| Operation can be done or remain undone| find min/max cost
+//problem_1: https://codeforces.com/contest/706/problem/C
+//solution_1: https://codeforces.com/contest/706/submission/90027194
