465e0fc515fb49d4cbe16913adb4d8091e1a9dc7
Naimul Islam <naim2018331019@gmail.com>
2020-08-27 08:51:36 +0600

    -

--- Segment Tree [My].cpp
+++ Segment Tree [My].cpp
@@ -4,15 +4,14 @@
     all means ALL :/
 */
 // recursive 
-#define Maxx 10000
+#define MAX 100000
 
-int T[Maxx*3 + 100];
+int T[MAX * 4 + 100];
 
 int query(int node, int beg, int end, int i, int j) {
-    if(beg >= i && end <= j)
-        return T[node];
-    if(end < i || beg > j)
-        return 0;
+    if(beg >= i && end <= j) return T[node];
+    if(end < i || beg > j) return 0;
+
     int mid = (beg + end) / 2;
     int p = query(2*node, beg, mid, i, j);
     int q = query(2*node + 1, mid + 1, end, i, j);
@@ -20,12 +19,12 @@ int query(int node, int beg, int end, int i, int j) {
 }
 
 void update(int node, int beg, int end, int i, int val) {
-    if(beg == i && end == i) {
+    if(beg == end) {  // beg == end == i
         T[node] += val;
         return;
     }
-    if(end < i || beg > i)
-        return;
+    if(end < i || beg > i) return;
+
     int mid = (beg + end) / 2;
     update(2*node, beg, mid, i, val);
     update(2*node + 1, mid+1, end, i, val);
@@ -36,32 +35,33 @@ void update(int node, int beg, int end, int i, int val) {
 
 
 //iterative [perfect!]
-const int N = 1e5;  // limit for array size
-int n;  // array size
-int t[2 * N];
+const int MAX = 1e5;  // limit for array size
+int start;  // next smallest power of 2
+int TT[4 * MAX + 100];
 
-void build() {  // build the tree
-  for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
+void build() {
+  for (int i = start - 1; i > 0; --i) TT[i] = TT[i<<1] + TT[i<<1|1];
 }
 
-void modify(int p, int value) {  // set value at position p
-  for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
+void modify(int p, int value) {  // set value at position p [0 based index]
+  for (TT[p += start] = value; p > 1; p >>= 1) TT[p>>1] = TT[p] + TT[p^1];
 }
 
-int query(int l, int r) {  // sum on interval [l, r)
+int query(int l, int r) {  // sum on interval [l, r) [0 base index]
   int res = 0;
-  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-    if (l&1) res += t[l++];
-    if (r&1) res += t[--r];
+  for (l += start, r += start; l < r; l >>= 1, r >>= 1) {
+    if (l&1) res += TT[l++];
+    if (r&1) res += TT[--r];
   }
   return res;
 }
 
 int main() {
-  scanf("%d", &n);
-  for (int i = 0; i < n; ++i) scanf("%d", t + n + i);
-  build();
-  modify(0, 1);
-  printf("%d\n", query(3, 11));
-  return 0;
+    cin >> n;
+    start = 1LL << int(ceil(log2(n)));
+    for (int i = 0; i < n; ++i) cin >> TT[start + i];
+    build();
+    modify(0, 1);
+    cout << query(3, 11)) << '\n';
+    return 0;
 }
