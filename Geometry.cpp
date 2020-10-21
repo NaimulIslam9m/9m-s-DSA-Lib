8c022c0b0fc7468333f3ce5df6cc8e6c9094e1fe
Naimul Islam <naim2018331019@gmail.com>
2020-10-21 21:59:42 +0600

    -

--- Geometry.cpp
+++ Geometry.cpp
@@ -1,41 +1,65 @@
-// Geometry
-struct point
-{
-	int x, y, idx;
+//*  checks if a shape can be fromed using some point
+struct Shape {
+	//! put it outside
+	struct Point
+	{
+		int x, y, idx;
+	    bool operator< (Point a) {
+			if (x == a.x)
+				return y < a.y;
+			return x < a.x;
+	    }
+	};
+
+	//Finds squared euclidean distance between two points
+	int dist(Point &a, Point &b)
+	{
+		return (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y);
+	}
+
+	//Checks if angle ABC is a right angle
+	bool isOrthogonal(Point &a, Point &b, Point &c)
+	{
+		return (b.x-a.x) * (b.x-c.x) + (b.y-a.y) * (b.y-c.y) == 0;
+	}
+
+	//Checks if ABCD form a rectangle (in that order)
+	bool isRectangle(Point &a, Point &b, Point &c, Point &d)
+	{
+		return isOrthogonal(a, b, c) && isOrthogonal(b, c, d) && isOrthogonal(c, d, a);
+	}
+
+	//Checks if ABCD form a rectangle, in any orientation
+	bool isRectangleAnyOrder(Point &a, Point &b, Point &c, Point &d)
+	{
+		return isRectangle(a, b, c, d) || isRectangle(b, c, a, d) || isRectangle(c, a, b, d);
+	}
+
+	//Checks if ABCD form a square (in that order)
+	bool isSquare(Point &a, Point &b, Point &c, Point &d)
+	{
+		return isRectangle(a, b, c, d) && dist(a, b) == dist(b, c);
+	}
+
+	//Checks if ABCD form a square, in any orientation
+	bool isSquareAnyOrder(Point &a, Point &b, Point &c, Point &d)
+	{
+		return isSquare(a, b, c, d) || isSquare(b, c, a, d) || isSquare(c, a, b, d);
+	}
 };
 
-//Finds squared euclidean distance between two points
-int dist(point &a, point &b)
-{
-	return (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y);
-}
 
-//Checks if angle ABC is a right angle
-bool isOrthogonal(point &a, point &b, point &c)
-{
-	return (b.x-a.x) * (b.x-c.x) + (b.y-a.y) * (b.y-c.y) == 0;
-}
+//* Take points and find distance between two points
+struct Point {
+    long double x = 0, y = 0;
+    Point() {}
+    Point(long double x, long double y = 0) :
+    x(x), y(y) {}
 
-//Checks if ABCD form a rectangle (in that order)
-bool isRectangle(point &a, point &b, point &c, point &d)
-{
-	return isOrthogonal(a, b, c) && isOrthogonal(b, c, d) && isOrthogonal(c, d, a);
-}
+    void read() { cin >> x >> y; }
 
-//Checks if ABCD form a rectangle, in any orientation
-bool isRectangleAnyOrder(point &a, point &b, point &c, point &d)
-{
-	return isRectangle(a, b, c, d) || isRectangle(b, c, a, d) || isRectangle(c, a, b, d);
-}
+    Point operator- (Point a) { return Point(x - a.x, y - a.y); }
 
-//Checks if ABCD form a square (in that order)
-bool isSquare(point &a, point &b, point &c, point &d)
-{
-	return isRectangle(a, b, c, d) && dist(a, b) == dist(b, c);
-}
-
-//Checks if ABCD form a square, in any orientation
-bool isSquareAnyOrder(point &a, point &b, point &c, point &d)
-{
-	return isSquare(a, b, c, d) || isSquare(b, c, a, d) || isSquare(c, a, b, d);
-}
+    long double mdist() { return x * x + y * y; } // manhattan distance
+    long double dist() { return sqrt(mdist()); } // euclidean distance
+};
